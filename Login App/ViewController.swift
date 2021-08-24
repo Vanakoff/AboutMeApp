1dbc3bfab232d162a5dba323518b3381d82be442
Vanakoff <andrey.vanakoff@gmail.com>
2021-08-25 00:22:41 +0200

    Functionality created, must be updated

--- Login App/ViewController.swift
+++ Login App/ViewController.swift
@@ -9,11 +9,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var usernameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    @IBOutlet var loginButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        loginButton.layer.cornerRadius = 10
     }
 
+    
+    @IBAction func loginButtonPressed() {
+        
+    }
+    
+    
+    @IBAction func forgotUsernameButtonPressed() {
+        showAlert(title: "Ooops!", message: "Username is User 😉")
+    }
+    
+    @IBAction func forgotPasswordButtonPressed() {
+        showAlert(title: "Ooops!", message: "Password is Password 😉")
+    }
+    
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let userVC = segue.destination as? UserViewController else { return }
+        userVC.name = "Welcome, \(usernameTextField.text ?? " ")!"
+    }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        usernameTextField.text?.removeAll()
+        passwordTextField.text?.removeAll()
+    }
+}
 
+// MARK: - private methods
+extension ViewController {
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
 }
 
